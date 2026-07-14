ae7980f0af5e1bf0de7e9ff6633361429f6ff32d
Hemang Lavana <hemanglavana@gmail.com>
2026-07-14 17:14:46 -0400

    Fix astro pkg build issues

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from 'astro/config';
-import githubPages from '@astrojs/github-pages';
 
 export default defineConfig({
-  site: 'https://www.sapala.fun',
-  adapter: githubPages(),
+  base: '/',
 });
